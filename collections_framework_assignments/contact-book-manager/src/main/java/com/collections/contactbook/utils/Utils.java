87846975bf08cba2b37bc267abfc433adbe46e74
Anubhav Aaryan <anubhav@fenado.ai>
2025-11-19 21:29:26 +0530

    fix: changed util from interface to class and added validation logic

--- collections_framework_assignments/contact-book-manager/src/main/java/com/collections/contactbook/utils/Utils.java
+++ collections_framework_assignments/contact-book-manager/src/main/java/com/collections/contactbook/utils/Utils.java
@@ -1,6 +1,22 @@
 package com.collections.contactbook.utils;
 
-public interface Utils {
-    public boolean validateEmail();
-    public boolean validatePhoneNumber();
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utils  {
+    private static final String _emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+    private static final String _phoneNumberRegex = "^\\d{10}$";
+    Utils(){super();}
+
+    public boolean validateEmail(String email){
+        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(_emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher match = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return match.matches();
+    }
+
+    public boolean validatePhone(String phone){
+        Pattern VALID_PHONE_REGEX = Pattern.compile(_phoneNumberRegex);
+        Matcher match = VALID_PHONE_REGEX.matcher(phone);
+        return match.matches();
+    }
 }
