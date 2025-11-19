63b989f1a3df995e5bd3d9c39371d8407de7fac4
Anubhav Aaryan <anubhav@fenado.ai>
2025-11-19 21:10:22 +0530

    feat: added contact service class

--- collections_framework_assignments/contact-book-manager/src/main/java/com/collections/contactbook/services/ContactService.java
+++ collections_framework_assignments/contact-book-manager/src/main/java/com/collections/contactbook/services/ContactService.java
@@ -1,7 +1,11 @@
 package com.collections.contactbook.services;
 
+import com.collections.contactbook.models.Contact;
 import org.springframework.stereotype.Service;
 import com.collections.contactbook.database.Database;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
@@ -9,7 +13,40 @@ public class ContactService {
     public ContactService(){}
 
     void addContact(String name, String phoneNumber, String email){
-        this._db.
+        boolean res = this._db.addContact(name, phoneNumber, email);
+        if(res){
+            Optional<Contact> c = this._db.getContactByPhone(phoneNumber);
+            if(c.isEmpty()){
+                System.out.println("Something went wrong");
+                return;
+            }
+            Contact contact = c.get();
+            System.out.println("New contact added!");
+            contact.print();
+        }
     }
 
+    void viewAllContacts(){
+        List<Contact> contacts = _db.getAllContacts();
+        _db.printContact(contacts);
+    }
+
+    void searchContacts(String phoneNumber){
+        Optional<Contact> c = _db.getContactByPhone(phoneNumber);
+        if(c.isEmpty()){
+            System.out.println("No contact exist with phone number "+phoneNumber);
+            return;
+        }
+        System.out.println("Contact found!");
+        c.get().print();
+    }
+
+    void deleteContact(String id){
+        boolean res = _db.deleteContact(id);
+        if(res){
+            System.out.println("Contact deleted successfully!");
+        }else{
+            System.out.println("Contact not found!");
+        }
+    }
 }
