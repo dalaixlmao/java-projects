d05f17fead452ab7649b7903a31d8aec71ffaf47
Anubhav Aaryan <anubhav@fenado.ai>
2025-11-19 20:05:15 +0530

    feat: added contact model

---
+++ collections_framework_assignments/contact-book-manager/src/main/java/com/collections/contactbook/models/Contact.java
@@ -0,0 +1,33 @@
+package com.collections.contactbook.models;
+
+/*
+ * - Contact
+ *  - id
+ *  - name
+ *  - phoneNumber
+ *  - email
+* */
+
+public class Contact {
+    private String _id;
+    private String _name;
+    private String _phoneNumber;
+    private String _email;
+
+    public Contact(String id, String name, String phoneNumber, String email){
+        this._id = id;
+        this._name = name;
+        this._phoneNumber = phoneNumber;
+        this._email = email;
+    }
+
+    public String getId() { return this._id; }
+    public String getName() { return this._name; }
+    public String getPhoneNumber() { return this._phoneNumber; }
+    public String getEmail() { return this._email; }
+
+    public void setId(String id) { this._id = id; }
+    public void setName(String name) { this._name = name; }
+    public void setPhoneNumber(String phoneNumber) { this._phoneNumber = phoneNumber; }
+    public void setEmail(String email) { this._email = email; }
+}
